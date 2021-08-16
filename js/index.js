5ce1cb4510352b01b1da03132a4d241f5301cc4f
12abdulmalek <abdul35-585@diu.edu.bd>
2021-08-16 08:51:54 +0600

    first cmnt

---
+++ js/index.js
@@ -0,0 +1,35 @@
+function getInputValue(inputValue){
+    const inputField=document.getElementById(inputValue);
+    const inputFieldText=inputField.value;
+    const inputFieldAmount=parseFloat(inputFieldText);
+    inputField.value='';
+    return inputFieldAmount;
+}
+function totalAmount1(input,inputvalue){
+    const depositAmount=document.getElementById(input);
+    const depositAmountText=depositAmount.innerText;
+    const depositAmountTotal=parseFloat(depositAmountText);
+    const totalDepositAmount=depositAmountTotal+inputvalue;
+    depositAmount.innerText=totalDepositAmount;
+}
+function upadateAmount(value,isadd){
+    const totalBalance=document.getElementById('total-balance');
+    const previousBalance=totalBalance.innerText;
+    const previusBalanceAmount=parseFloat(previousBalance);   
+    if(isadd==true){
+        totalBalance.innerText=previusBalanceAmount+value;
+    }
+    else{
+        totalBalance.innerText=previusBalanceAmount-value;
+    }
+}
+document.getElementById('deposit-button').addEventListener('click',function(){
+    const depositAmount1= getInputValue('deposit-input');
+    totalAmount1('deposit-amount',depositAmount1)
+    upadateAmount(depositAmount1 ,true);
+})
+document.getElementById('withdraw-button').addEventListener('click',function(){
+    const withdrawInputAmount = getInputValue('withdraw-input');
+    totalAmount1('withdraw-amount',withdrawInputAmount);
+    upadateAmount( withdrawInputAmount ,false);
+})
